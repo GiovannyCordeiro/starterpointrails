946e067984b57587352e13cb23453f5f1e6cdf22
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-02-26 17:21:46 -0300

    forma correta de declarar multiplas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,5 @@ Rails.application.routes.draw do
   # second form
   root 'home#index'
 
-  get '/vehicles', controller: 'vehicles', action: 'index'
-
-  get '/vehicles/new', controller: 'vehicles', action: 'new'
-
-  post '/vehicles', controller: 'vehicles', action: 'create'
-
-  get '/vehicles/:id', controller: 'vehicles', action: 'show'
-
-  get '/vehicles/:id/edit', controller: 'vehicles', action: 'edit'
-
-  put '/vehicles/:id', controller: 'vehicles', action: 'update'
-  patch '/vehicles/:id', controller: 'vehicles', action: 'update'
-
-  delete '/vehicles/:id', controller: 'vehicles', action: 'destroy'
+  resources :vehicles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
